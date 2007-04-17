61e90c9608ec4f49e46e4bd813d26ed1114f1631
hmelis <hmelis@68fc37a4-136d-11dd-a829-b31a51d74fb4>
2007-04-17 16:24:53 +0000

    - Fixed: site.ini missing to signal this is an extension with translations - Tagged version 2.5

--- trunk/extension/regexpline/ezinfo.php
+++ trunk/extension/regexpline/ezinfo.php
@@ -6,7 +6,7 @@ class regexplineInfo
     {
         return array(
             'Name' => 'Regular Expression Datatype',
-            'Version' => '2.4',
+            'Version' => '2.5',
             'Copyright' => 'Copyright (C) 2005-' . date( 'Y' ) . ' Hans Melis',
             'License' => 'GNU General Public License v2.0'
         );
