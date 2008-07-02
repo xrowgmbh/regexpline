993b5e6d2ba6f5a2822e6122cf02e9b6aaaf79d5
xrow <xrow@68fc37a4-136d-11dd-a829-b31a51d74fb4>
2008-07-02 15:22:08 +0000

    - german translation added

---
+++ trunk/translations/ger/translation.ts
@@ -0,0 +1,175 @@
+<!DOCTYPE TS><TS>
+  <context>
+    <name>extension/regexpline/datatype</name>
+    <message>
+      <source>Regular Expression Text</source>
+      <comment>Datatype name</comment>
+      <translation>Regular Expression Text</translation>
+      <translated>Regular Expression Text</translated>
+    </message>
+    <message>
+      <source>Your input did not meet the requirements.</source>
+      <translation>Your input did not meet the requirements.</translation>
+      <translated>Your input did not meet the requirements.</translated>
+    </message>
+    <message>
+      <source>This is a required field.</source>
+      <translation>This is a required field.</translation>
+      <translated>This is a required field.</translated>
+    </message>
+    <message>
+      <source>You need at least one regular expression or selected preset</source>
+      <translation>You need at least one regular expression or selected preset</translation>
+      <translated>You need at least one regular expression or selected preset</translated>
+    </message>
+    <message>
+      <source>The regular expression &quot;%1&quot; is invalid</source>
+      <translation>The regular expression &quot;%1&quot; is invalid</translation>
+      <translated>The regular expression &quot;%1&quot; is invalid</translated>
+    </message>
+  </context>
+  <context>
+    <name>extension/regexpline/design/standard/class/datatype/edit</name>
+    <message>
+      <source>To allow all input:</source>
+      <translation>To allow all input:</translation>
+      <translated>To allow all input:</translated>
+    </message>
+    <message>
+      <source>Presets</source>
+      <translation>Presets</translation>
+      <translated>Presets</translated>
+    </message>
+    <message>
+      <source>Help text for users</source>
+      <translation>Help text for users</translation>
+      <translated>Help text for users</translated>
+    </message>
+    <message>
+      <source>No subpatterns defined. Using the complete expression.</source>
+      <translation>No subpatterns defined. Using the complete expression.</translation>
+      <translated>No subpatterns defined. Using the complete expression.</translated>
+    </message>
+    <message>
+      <source>Object name pattern</source>
+      <translation>Object name pattern</translation>
+      <translated>Object name pattern</translated>
+    </message>
+    <message>
+      <source>This field allows you to structure the object naming pattern for this attribute. To use a subpattern of your regular expression, place its number (visible in the list below) in a tag-like notation, e.g. &amp;lt;1&amp;gt; to use the first subpattern.</source>
+      <translation>This field allows you to structure the object naming pattern for this attribute. To use a subpattern of your regular expression, place its number (visible in the list below) in a tag-like notation, e.g. &amp;lt;1&amp;gt; to use the first subpattern.</translation>
+      <translated>This field allows you to structure the object naming pattern for this attribute. To use a subpattern of your regular expression, place its number (visible in the list below) in a tag-like notation, e.g. &amp;lt;1&amp;gt; to use the first subpattern.</translated>
+    </message>
+    <message>
+      <source>These are the available subpatterns:</source>
+      <translation>These are the available subpatterns:</translation>
+      <translated>These are the available subpatterns:</translated>
+    </message>
+    <message>
+      <source>Expression</source>
+      <translation>Expression</translation>
+      <translated>Expression</translated>
+    </message>
+    <message>
+      <source>Error message</source>
+      <translation>Error message</translation>
+      <translated>Error message</translated>
+    </message>
+    <message>
+      <source>Regular expression(s) (Perl-compatible)</source>
+      <translation>Regular expression(s) (Perl-compatible)</translation>
+      <translated>Regular expression(s) (Perl-compatible)</translated>
+    </message>
+    <message>
+      <source>Add regular expression</source>
+      <translation>Add regular expression</translation>
+      <translated>Add regular expression</translated>
+    </message>
+    <message>
+      <source>Remove regular expression(s)</source>
+      <translation>Remove regular expression(s)</translation>
+      <translated>Remove regular expression(s)</translated>
+    </message>
+    <message>
+      <source>Display as</source>
+      <translation>Display as</translation>
+      <translated>Display as</translated>
+    </message>
+    <message>
+      <source>Single text line</source>
+      <translation>Single text line</translation>
+      <translated>Single text line</translated>
+    </message>
+    <message>
+      <source>Text area (Text block)</source>
+      <translation>Text area (Text block)</translation>
+      <translated>Text area (Text block)</translated>
+    </message>
+    <message>
+      <source>Regexp / Preset</source>
+      <translation>Regexp / Preset</translation>
+      <translated>Regexp / Preset</translated>
+    </message>
+    <message>
+      <source>Negate</source>
+      <translation>Negate</translation>
+      <translated>Negate</translated>
+    </message>
+    <message>
+      <source>The following errors occured:</source>
+      <translation>The following errors occured:</translation>
+      <translated>The following errors occured:</translated>
+    </message>
+  </context>
+  <context>
+    <name>extension/regexpline/design/standard/class/datatype/view</name>
+    <message>
+      <source>Regular expression</source>
+      <translation>Regular expression</translation>
+      <translated>Regular expression</translated>
+    </message>
+    <message>
+      <source>Help text</source>
+      <translation>Help text</translation>
+      <translated>Help text</translated>
+    </message>
+    <message>
+      <source>Object name pattern</source>
+      <translation>Object name pattern</translation>
+      <translated>Object name pattern</translated>
+    </message>
+    <message>
+      <source>No pattern supplied. Using the complete expression.</source>
+      <translation>No pattern supplied. Using the complete expression.</translation>
+      <translated>No pattern supplied. Using the complete expression.</translated>
+    </message>
+    <message>
+      <source>Display type</source>
+      <translation>Display type</translation>
+      <translated>Display type</translated>
+    </message>
+    <message>
+      <source>Identifier</source>
+      <translation>Identifier</translation>
+      <translated>Identifier</translated>
+    </message>
+    <message>
+      <source>Negated</source>
+      <translation>Negated</translation>
+      <translated>Negated</translated>
+    </message>
+    <message>
+      <source>Error message</source>
+      <translation>Error message</translation>
+      <translated>Error message</translated>
+    </message>
+  </context>
+  <context>
+    <name>extension/regexpline/design/standard/content/datatype/edit</name>
+    <message>
+      <source>Help text</source>
+      <translation>Help text</translation>
+      <translated>Help text</translated>
+    </message>
+  </context>
+</TS>
